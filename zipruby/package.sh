d81d01042a18411d6b2a25ddaf8b5e95bb904d80
winebarrel <none@none>
2009-04-19 01:27:50 +0000

    r3417@LENOVO-6D4C2336:  sugawara | 2009-04-19 10:22:27 +0900

--- zipruby/package.sh
+++ zipruby/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 VERSION=0.3.0
 
-rm *.gem *.tar.bz2
+rm *.gem *.tar.bz2 2> /dev/null
 rm -rf doc
 echo "/* rdoc source */" > zipruby.c
 for i in ext/*.[ch]
@@ -11,12 +11,21 @@ done
 cat ext/*.c >> zipruby.c
 cp ../libzip/*.{c,h} ext
 rdoc README.txt zipruby.c LICENSE.libzip --title 'Zip/Ruby - Ruby bindings for libzip.'
-tar jcvf zipruby-${VERSION}.tar.bz2 --exclude=.svn README.txt *.gemspec ext doc
+mkdir work
+cp -r * work 2> /dev/null
+cd work
+tar jcf zipruby-${VERSION}.tar.bz2 --exclude=.svn README.txt *.gemspec ext doc
 gem build zipruby.gemspec
 gem build zipruby-mswin32.gemspec
 cp zipruby-${VERSION}-x86-mswin32.gem zipruby-${VERSION}-mswin32.gem
+rm -rf lib
+mv lib1.9 lib
+gem build zipruby1.9-mswin32.gemspec
 rm zipruby.c
+cp *.gem *.tar.bz2 ..
+cd ..
 for i in `ls ../libzip/*.{c,h}`
 do
   rm ext/`basename $i`
 done
+rm -rf work
